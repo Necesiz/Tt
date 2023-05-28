78d4692da0dda752ecf706f4fce5f7e85eb38afa
Teamabasof <104113926+Teamabasof@users.noreply.github.com>
2023-05-28 19:37:39 +0400

    Update Config.py

--- Config.py
+++ Config.py
@@ -7,9 +7,9 @@ class Config():
     admins = {}
     API_ID = int(os.environ.get("API_ID","15954332"))
     API_HASH = os.environ.get("API_HASH","85adea6f1eaf068b707703b4846a9ced")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5964973513:AAGpzC4M4RP7uujfZc_PpXuMlt4iFqezdus")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "OldMultiBot")
-    BOT_NAME = os.environ.get("BOT_NAME", "OLD MULTİ")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6220871532:AAEeZBDvusB_TZRBwF9FeXNuWVQjKueNjjY")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "PersonalAssistanttBot")
+    BOT_NAME = os.environ.get("BOT_NAME", "PERSONAL ASSISTANT")
     BOT_ID = int(os.environ.get("BOT_ID", "5964973513"))
     SUDO_USERS = os.environ.get("SUDO_USERS", "5134595693").split()
     SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "oldsupport")
